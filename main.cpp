060756a819e3edbb2d7c52eb59c75501583e18db
yourcomrade <MinhXecole@gmail.com>
2022-04-23 21:51:51 +0200

    my project

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "IntArray.h"
+
+using namespace std;
+int main() {
+    const int SIZE=10;
+    IntArray table(SIZE);
+    for(int i=0;i<SIZE;i++)
+        table[i]=(i*2);
+    for (int i = 0; i <SIZE ; i++) {
+        cout<<table[i]<<" ";
+    }
+    cout<<"\n";
+    for(int i=0;i<SIZE;i++)
+        table[i]=table[i]+5;
+    for(int i=0;i<SIZE;i++)
+        cout<<table[i]<<" ";
+    cout<<"\n";
+    for(int i=0;i<SIZE;i++)
+        table[i]++;
+    for (int S = 0; S < SIZE; ++S) {
+        cout<<table[S]<<" ";
+    }
+    cout<<"\n";
+    return 0;
+}
